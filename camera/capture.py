a3121f7bbe1444d378472ee56b75f7f4c194b79c
MakkaiNandor <makkainandor@gmail.com>
2023-07-02 22:16:35 +0300

    camera modul updated

--- camera/capture.py
+++ camera/capture.py
@@ -2,17 +2,53 @@ import cv2
 import time
 
 class CameraCapture():
+    """
+    Video capture class that reads the frames from the video source (file or camera) and retrieve them for further processing.
+
+    Usage:
+        cap = CameraCapture(0)
+        cap.start()
+        width, height = cap.captureSize
+        while cap.isCaptureOpened:
+            fps = cap.fps
+            timestamp = cap.timestampMs
+
+            if not cap.readNextFrame():
+                break
+            
+            frame = cap.frame
+            # further image processing
+        cap.release()
+    """
+
     def __init__(self, video_source):
-        self._capture = cv2.VideoCapture(video_source)
-        self._frame = None
+        """Constructor
         
-        self._capture_size = (self._capture.get(cv2.CAP_PROP_FRAME_WIDTH), self._capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        Parameters:
+        -----------
+        video_source
+            video file path or camera id
+        """
+        self._source = video_source
+        self._capture = cv2.VideoCapture()
+        self._frame = None
         
         self._start_time = None
         self._prev_frame_time = None
         self._frame_time = None
 
+    def start(self):
+        """Start the VideoCapture object"""
+        if not self.isCaptureOpened:
+            self._capture.open(self._source)
+
+    def release(self):
+        """Release the VideoCapture object"""
+        if self.isCaptureOpened:
+            self._capture.release()
+
     def readNextFrame(self):
+        """Read the next frame"""
         if not self.isCaptureOpened:
             return False
 
@@ -28,6 +64,7 @@ class CameraCapture():
 
     @property
     def frame(self):
+        """Retrieve the previously read frame"""
         if not self.isCaptureOpened:
             return None
 
@@ -35,18 +72,22 @@ class CameraCapture():
         
     @property
     def fps(self):
+        """Retrieve the calculated FPS"""
         if self._prev_frame_time is not None and self._frame_time is not None:
             return str(int(1 / (self._frame_time - self._prev_frame_time)))
         return None
     
     @property
     def isCaptureOpened(self):
+        """Check if the VideoCapture object is opened"""
         return self._capture.isOpened()
 
     @property
     def captureSize(self):
-        return self._capture_size
+        """Retrieve the video source dimensions (width and height)"""
+        return (self._capture.get(cv2.CAP_PROP_FRAME_WIDTH), self._capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
     @property
     def timestampMs(self):
+        """Retrieve the absolute timestamp in milliseconds"""
         return int((time.time() - self._start_time) * 1000)
--- camera/window.py
+++ camera/window.py
@@ -1,7 +1,46 @@
 import cv2
+from utils import *
 
 class WindowManager():
-    def __init__(self, window_name, keypress_callback = None, show_fps=None):
+    """
+    Basic window manager class that creates a named window and shows images within that. It can handle key press events and optionally can show the FPS.
+
+    Usage:
+        def callback(key):
+            if key == 27:
+                win.destroyWindow()
+
+        def asyncProcess():
+            image = ...
+            win.setFrame(image)
+
+        win = WindowManager('Main Window', callback)
+        win.createWindow()
+        while win.isWindowOpened:
+            # 1. Synchronous way
+            image = ...
+            fps = ...
+            win.show(image, fps)
+
+            # 2. Asynchronous way
+            asyncProcess()
+            fps = ...
+            win.showFrame(fps)
+
+            win.processEvents()
+        win.destroyWindow()
+    """
+
+    def __init__(self, window_name, keypress_callback = None):
+        """Constructor
+        
+        Parameters:
+        -----------
+        window_name
+            the name of the window
+        keypress_callback (default: None)
+            the callback that handles the keypress event
+        """
         self.keypress_callback = keypress_callback
         self._window_name = window_name
         self._window_created = False
@@ -9,34 +48,73 @@ class WindowManager():
 
     @property
     def isWindowCreated(self):
+        """Check if the window is created"""
         return self._window_created
     
     @property
     def isWindowOpened(self):
-        return self._window_created and cv2.getWindowProperty(self._window_name, cv2.WND_PROP_VISIBLE) >= 1
+        """Check if the window is opened"""
+        if not self._window_created:
+            return False
+
+        try:
+            cv2.getWindowProperty(self._window_name, cv2.WND_PROP_VISIBLE)
+            opened = True
+        except:
+            opened = False
+
+        return opened
     
     def createWindow(self):
-        cv2.namedWindow(self._window_name)
-        self._window_created = True
+        """Create the window"""
+        if not self.isWindowCreated:
+            cv2.namedWindow(self._window_name)
+            self._window_created = True
+
+    def destroyWindow(self):
+        """Destroy the window"""
+        if self.isWindowCreated:
+            cv2.destroyWindow(self._window_name)
+            self._window_created = False
 
     def setFrame(self, frame):
+        """Set the frame which should be shown next in the window
+        
+        Parameters:
+        -----------
+        frame
+            the image to set
+        """
         self._frame_to_show = frame
 
     def showFrame(self, fps=None):
+        """Show the frame
+        
+        Parameters:
+        -----------
+        fps (default: None)
+            number of FPS to show
+        """
         if self._frame_to_show is not None:
             self.show(self._frame_to_show, fps)
             self._frame_to_show = None
 
     def show(self, frame, fps=None):
+        """Show an image in the window
+
+        Parameters:
+        -----------
+        frame
+            the image to show
+        fps (default: None)
+            number of FPS to show
+        """
         if fps is not None:
-            cv2.putText(frame, fps, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 3, cv2.LINE_AA)
+            putTextOnImage(frame, fps, (10, 30), COLOR_BLUE)
         cv2.imshow(self._window_name, frame)
 
-    def destroyWindow(self):
-        cv2.destroyWindow(self._window_name)
-        self._window_created = False
-
     def processEvents(self):
+        """Call the keypress_callback to handle events"""
         keycode = cv2.waitKey(1)
         if self.keypress_callback is not None and keycode != -1:
             self.keypress_callback(keycode & 0xFF)
